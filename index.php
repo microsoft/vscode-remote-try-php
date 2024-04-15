303d6d6916046e3bb90783d0b2479f7e16754472
Tuananhdeptrai723 <96807994+Tuananhdeptrai723@users.noreply.github.com>
2024-04-15 03:05:37 +0000

    Chia Folder

--- index.php
+++ index.php
@@ -9,13 +9,22 @@ function sayHello($name) {
 	echo "Hello $name!";
 }
 
+
+	$stukhoa = $_REQUEST["txtTuKhoa"];
+
 ?>
 
+<script>
+	function showMessage() {
+		alert("You clicked the button!");
+	};
+</script>
 <html>
 	<head>
 		<title>PHP course</title>
 	</head>
 	<body>
+		<button onclick="showMessage()">Click Me</button>
 		<?php 
 		
 		sayHello('= xin chao ');
