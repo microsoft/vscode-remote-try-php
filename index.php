b0ad2382acd767fd1dfca2c9e78f612ef7497623
pphuc2205 <89580343+pphuc2205@users.noreply.github.com>
2024-04-15 08:36:21 +0000

    upload ex1 and ex2

--- index.php
+++ index.php
@@ -1,27 +1,39 @@
-<?php
+<!-- //<?php
 
 /*----------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
+// function sayHello($name) {
+// 	echo "Hello $name!";
+// }
 
-?>
+?> -->
 
 <html>
 	<head>
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
+		<h2>Ví dụ 1:</h2>
+		<p>Văn bản HTML.</p>
 		<?php 
 		
-		sayHello('Phvcs_Lep, TrinhDacThai');
-			
-		phpinfo(); 
+		echo '<p>Văn bản PHP!</p>';
 			
 		?>
+		<p>Văn bản khác.</p>
+
+		<h2>Ví dụ 2:</h2>
+		<?php
+
+			echo '<p>Khối dữ liệu PHP 1.</p>';
+
+		?>
+		<p>dữ liệu HTML, <?php echo 'Văn bản PHP!'; ?></p>
+		<?php echo'<b>'; ?>
+			Một ví dụ kết hợp HTML & PHP
+		<?php echo'</b>'; ?>
 	</body>
 </html>
