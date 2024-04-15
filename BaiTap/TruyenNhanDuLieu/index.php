e3f7f80902f401d82fa91762e1170f7f1be57b8e
Tuananhdeptrai723 <96807994+Tuananhdeptrai723@users.noreply.github.com>
2024-04-15 03:44:59 +0000

    test branch

---
+++ BaiTap/TruyenNhanDuLieu/index.php
@@ -0,0 +1,22 @@
+<?php
+
+function sayHello($name) {
+	echo "Hello $name!";
+}
+
+?>
+
+<html>
+	<head>
+		<title>Visual Studio Code Remote :: PHP</title>
+	</head>
+	<body>
+		<?php 
+		
+		sayHello('Tuan Anh');
+			
+		phpinfo(); 
+			
+		?>
+	</body>
+</html>
