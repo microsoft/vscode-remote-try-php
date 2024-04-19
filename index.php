d89ebb1547cdd1ec560889ad399dabebbc88bfa4
DucAnh <anhnguyen2k373@gmail.com>
2024-04-19 13:51:19 +0700

    add file

--- index.php
+++ index.php
@@ -10,18 +10,7 @@
 	</head>
 	<body>
 	<?php 
-		$so_luong = $_POST["so_luong"];
-        $don_gia = $_POST["don_gia"];
-        if ($so_luong < 10)
-        {
-            $thanh_tien = $don_gia * $so_luong;
-        } elseif ($so_luong > 10 and $so_luong < 20)
-        {
-            $thanh_tien = $don_gia * $so_luong * 0.95;
-        } else {
-            $thanh_tien = $don_gia * $so_luong * 0.9;
-        }
-		// phpinfo(); 
+		
 	?>
 	</body>
 </html>
