ca53a6559efe13344a49206e538ed2ae6b2feedf
Lại Tiến Đức <94907773+Tienduc37y@users.noreply.github.com>
2024-04-19 06:51:01 +0000

    echo print printf php example

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
-<?php
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
 
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
+		<h2>BT1</h2>
+		<!-- <?php
+		for ($i = 1; $i <= 200; $i++) {
+			if ($i % 2 == 0) {
+				echo '<span style="color: red; font-weight: bold;">' . $i . '</span> ';
+			} else {
+				echo '<span style="color: blue; font-style: italic;">' . $i . '</span> ';
+			}
+		}
+		?> -->
+		<div>
+			<?php
+				$Ngonngu1="PHP";
+				$Ngonngu2="ASP.NET";
+				echo "<p>$Ngonngu1 và $Ngonngu2 là ngôn ngữ WebServer.</p>";
+				print "<p>$Ngonngu1 và $Ngonngu2 là ngôn ngữ WebServer.</p>";
+				printf("%s và %s là ngôn ngữ WebServer",$Ngonngu1,$Ngonngu2);
+			?>
+		</div>
+
 	</body>
 </html>
