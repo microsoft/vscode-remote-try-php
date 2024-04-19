3df2de08c0cd39e76bba7b2969c209cd681113af
Lại Tiến Đức <94907773+Tienduc37y@users.noreply.github.com>
2024-04-19 07:03:12 +0000

    example array

--- index.php
+++ index.php
@@ -16,13 +16,30 @@
 			}
 		}
 		?> -->
-		<div>
+		<!-- <div>
 			<?php
 				$Ngonngu1="PHP";
 				$Ngonngu2="ASP.NET";
 				echo "<p>$Ngonngu1 và $Ngonngu2 là ngôn ngữ WebServer.</p>";
 				print "<p>$Ngonngu1 và $Ngonngu2 là ngôn ngữ WebServer.</p>";
 				printf("%s và %s là ngôn ngữ WebServer",$Ngonngu1,$Ngonngu2);
+			?>
+		</div> -->
+		<div>
+			<?php
+				$n = 100;
+				$arr1 = array($n);
+				$arr2 = array(1,2,3,4);
+				$arr3 = array();
+				$arr4 = array("hoten" => "Tiến Đức",
+							"quequan" => "Ha Nội",
+							"tuoi" => 22			
+				);
+				print_r($n);
+				print_r($arr1);
+				print_r($arr2);
+				print_r($arr3);
+				print_r($arr4);
 			?>
 		</div>
 
