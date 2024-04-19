6e3129e2291f9d99ea277ed9f99fb87d1244046d
DucAnh <anhnguyen2k373@gmail.com>
2024-04-19 14:09:25 +0700

    Cấu trúc rẽ nhánh if

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 
 <?php
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
 
 ?>
 <html>
@@ -12,33 +8,17 @@
 	</head>
 	<body>
 	<?php 
-		// tạo và in một mảng
-		$array = array(1, 2, 3, 4, 5);
-		print_r($array); // in ra chỉ số mảng và các giá trị tương ứng
-		$n = 100;
-		$arr1 = array($n); // mảng chỉ có một phần tử với biến n
-		echo "arr2: \n";
-		print_r($arr1);
-		echo "<br>";
-		$arr2 = array();
-		for($i = 0; $i < $n; $i++) {
-			$arr2[] = "giá trị " . $i . "<br>";
-		}
-		echo "arr2: \n";
-		print_r($arr2); // mảng có 100 phần tử với n giá trị
-
-		echo "<br>";
-		$arr3 = array(
-			'hoten' => 'Nguyễn Đức Anh',
-			'quequan' => 'Thanh Hóa',
-			'tuoi' => '21',
-			'IQ' => 'Rất cao'
-		);
-		echo "arr3:\n";
-		print_r($arr3); // mảng kết hợp
-
-
-
+		$so_luong = $_POST["so_luong"];
+        $don_gia = $_POST["don_gia"];
+        if ($so_luong < 10)
+        {
+            $thanh_tien = $don_gia * $so_luong;
+        } elseif ($so_luong >= 10 and $so_luong <= 20)
+        {
+            $thanh_tien = $don_gia * $so_luong * 0.95;
+        } else {
+            $thanh_tien = $don_gia * $so_luong * 0.9;
+        }
 		// phpinfo(); 
 	?>
 	</body>
