995ee95df138ba26e9694739ca28d0e3de53cb7c
vuminhphong23 <124127169+vuminhphong23@users.noreply.github.com>
2024-04-19 06:58:04 +0000

    timsach

--- index.php
+++ index.php
@@ -1,27 +1,48 @@
 <?php
+
 /*----------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
+
+function sayHello($name) {
+	echo "Hello $name!";
+}
+
 ?>
+
 <html>
 	<head>
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-	<?php 
-		$so_luong = $_POST["so_luong"];
-        $don_gia = $_POST["don_gia"];
-        if ($so_luong < 10)
-        {
-            $thanh_tien = $don_gia * $so_luong;
-        } elseif ($so_luong > 10 and $so_luong < 20)
-        {
-            $thanh_tien = $don_gia * $so_luong * 0.95;
-        } else {
-            $thanh_tien = $don_gia * $so_luong * 0.9;
-        }
-		// phpinfo(); 
-	?>
+		
+		<p>Văn bản HTML.</p>
+		<?php 
+			echo '<p>Văn bản PHP</p>'
+		?>
+		<p>Văn bản HTML khác</p>
+
+		<?php 
+			echo '<p>Khối dữ liệu PHP1</p>'
+		?>
+		<p>Dữ liệu HTML, <?php echo 'Dữ liệu PHP2' ?></p>
+		<?php echo '<b>'; ?>
+			Một ví dụ kết hợp HTNL và PHP
+		<?php echo '</b>'; ?>
+		<br><br>
+		<form action="xltimsach.php" Method="GET" >
+        Từ khóa : <input type="text" name="txtTukhoa"/>
+        <input type="submit" value="Tìm"/>
+        </form>
+		<br><br>
+		<?php
+			define("PI", 3.14);
+			$r=10;
+			$s = PI * pow($r,2);
+			$p = 2*PI * $r;
+			printf("$%.2f", $s);
+		?>
+
 	</body>
 </html>
