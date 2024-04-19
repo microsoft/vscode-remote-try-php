7a385734cea8396a487795bf01a0403427d8d77b
nmhung03 <manhhung3072003@gmail.com>
2024-04-19 17:54:36 +0700

    submit

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Web nâng cao</title>
+</head>
+
+<style>
+	
+</style>
+
+<body>
+	
+</body>
 
 </html>
