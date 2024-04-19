48a8c2ffe64f824b43810f6643878bc2b4f3b61e
DucAnh <anhnguyen2k373@gmail.com>
2024-04-19 12:08:56 +0700

    Bài tập Tìm sách

--- index.php
+++ index.php
@@ -1,41 +1,35 @@
 <?php
-
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function func($value) {
-	echo "Hello $value";
-}
-
-function func_Local() {
-	$a = 5;
-	echo $a;
-}
-
-function func_test() {
-	static $a = 0;
-	echo "Biến static ";
-	echo "$a<br>";
-	$a++;
+// Xử lý biểu mẫu tìm kiếm khi được gửi đi
+if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["txtTukhoa"])) {
+    $sTukhoa = htmlspecialchars($_GET["txtTukhoa"]);
+    $searched = true;
+} else {
+    $searched = false;
 }
 ?>
 
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-	<?php 
-		// Trong PHP tham chiếu có nghĩa là lấy cùng một giá trị bằng nhiều tên biến khác nhau.
-		// Ký hiệu tham chiếu: &
-		$a = 10;
-		$b = &$a;
-		//take value
-		echo $a; 
-		echo $b;
-		// phpinfo(); 
-	?>
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tìm sách</title>
+</head>
+<body>
+    <h1>TÌM SÁCH</h1>
+
+    <?php if ($searched) { ?>
+        <p>Từ khóa tìm sách là: <?= $sTukhoa ?></p>
+        <p>Kết quả tìm là:</p>
+        <ul>
+            <!-- Thêm logic hiển thị kết quả tìm kiếm ở đây -->
+        </ul>
+    <?php } else { ?>
+        <form action="index.php" method="GET">
+            <label for="txtTukhoa">Từ khóa:</label>
+            <input type="text" id="txtTukhoa" name="txtTukhoa">
+            <input type="submit" value="Tìm">
+        </form>
+    <?php } ?>
+</body>
 </html>
