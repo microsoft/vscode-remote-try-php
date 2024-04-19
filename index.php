529eec3e3bc039a12fb74f023c533cb5f711cdf2
pphuc2205 <89580343+pphuc2205@users.noreply.github.com>
2024-04-19 07:56:05 +0000

    Bai tap chan le

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-		<h2>Ví dụ 1:</h2>
+	<h2>Ví dụ 1:</h2>
 		<p>Văn bản HTML.</p>
 		<?php 
 		
@@ -31,9 +31,42 @@
 			echo '<p>Khối dữ liệu PHP 1.</p>';
 
 		?>
-		<p>dữ liệu HTML, <?php echo 'Văn bản PHP!'; ?></p>
+		<p>Dữ liệu HTML, <?php echo 'Dữ liệu PHP 2.'; ?></p>
 		<?php echo'<b>'; ?>
 			Một ví dụ kết hợp HTML & PHP
 		<?php echo'</b>'; ?>
+
+		<?php
+			$NgonNgu1 = "PHP";
+			$Ngonngu2 = "ASP .NET";
+			echo $NgonNgu1, "và", $Ngonngu2, "là ngôn ngữ lập trình WEBSITE";
+
+		?>
+
+
+		<?php
+			$num1 = 10;
+			$num2 = 20;
+			printf("%d+%d=%d",$num1,$num2,$num1+$num2 );
+
+			define("PI", 3.14);
+			$r=10;
+			$s= PI * pow($r ,2);
+			$p = 2 * PI * $r;
+		?>
+
+		<?php
+
+			for($i = 1; $i <= 200; $i++){
+				if ($i%2==0){
+					echo'<b style = "Color: red">' . $i . '</b>';
+				}
+					
+				else {
+					echo'<i style = "Color: blue">'. $i . '</i>';
+				}
+					
+			}
+		?>
 	</body>
 </html>
