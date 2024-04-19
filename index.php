5453502a6f678ac0925d3c7bebafed780e4a7052
DucAnh <anhnguyen2k373@gmail.com>
2024-04-19 08:19:30 +0700

    Biến static

--- index.php
+++ index.php
@@ -5,9 +5,22 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
 
-function sayHello($name) {
-	echo "Hello $name!";
+function func($value) {
+	echo "Hello $value";
 }
+
+function func_Local() {
+	$a = 5;
+	echo $a;
+}
+
+function func_test() {
+	static $a = 0;
+	echo "Biến static ";
+	echo "$a<br>";
+	$a++;
+}
+
 
 ?>
 
@@ -17,11 +30,13 @@ function sayHello($name) {
 	</head>
 	<body>
 		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
+		$value = 100;
+		// func($value);
+		// func_Local();
+		func_test();
+		func_test();
+		func_test();
+		// phpinfo(); 
 		?>
 	</body>
 </html>
