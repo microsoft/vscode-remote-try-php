057b7c6005a61ffd622101d5aadc7e284ab4fc71
vuminhphong23 <124127169+vuminhphong23@users.noreply.github.com>
2024-04-19 06:50:04 +0000

    19042024

--- index.php
+++ index.php
@@ -16,12 +16,34 @@ function sayHello($name) {
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-		<?php 
 		
-		sayHello('K15-N03');
-			
-		phpinfo(); 
-			
+		<p>Văn bản HTML.</p>
+		<?php 
+			echo '<p>Văn bản PHP</p>'
+		?>
+		<p>Văn bản HTML khác</p>
+
+		<?php 
+			echo '<p>Khối dữ liệu PHP1</p>'
+		?>
+		<p>Dữ liệu HTML, <?php echo 'Dữ liệu PHP2' ?></p>
+		<?php echo '<b>'; ?>
+			Một ví dụ kết hợp HTNL và PHP
+		<?php echo '</b>'; ?>
+
+		<h1>Tìm sách</h1>
+		<form action = "xlTimSach.phd" method="GET">
+			Từ khóa: <input type="text" name="txtTukhoa"/>
+			<input type=submit name=submit value="Tim">
+		</form>
+		<?php
+			$sTukhoa = $_REQUEST["txtTukhoa"];
+			if (isset($sTukhoa))
+			{
+			print "Từ khóa tìm sách là : $sTukhoa";
+			echo "<br>Kết quả tìm là : ";
+			}
 		?>
 	</body>
+
 </html>
