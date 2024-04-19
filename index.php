fe73113ae2e30f9c0a7249a5364a3c78744bd1dd
nmhung03 <manhhung3072003@gmail.com>
2024-04-19 16:50:39 +0700

    submit

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
 </html>
