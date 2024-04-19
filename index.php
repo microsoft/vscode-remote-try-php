3f5b88cde4f71ce8ff976d0c7baa6ed3d30e184d
DucAnh <anhnguyen2k373@gmail.com>
2024-04-19 12:21:48 +0700

    Bài tập Login

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 // Xử lý biểu mẫu tìm kiếm khi được gửi đi
-if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["txtTukhoa"])) {
-    $sTukhoa = htmlspecialchars($_GET["txtTukhoa"]);
-    $searched = true;
+if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["txtTenTaiKhoan"]) && isset($_GET["txtMatKhau"])) {
+    $username = $_GET["txtTenTaiKhoan"];
+    $password = $_GET["txtMatKhau"];
+
+    // Kiểm tra thông tin đăng nhập
+    if ($username === "admin" && $password === "12345") {
+        $message = "Welcome, admin";
+        $messageStyle = 'style="font-family: Tahoma; color: red;"';
+    } else {
+        $message = "Tên tài khoản hoặc Mật khẩu sai. Vui lòng nhập lại!";
+        $messageStyle = '';
+    }
+
+    $authenticated = true;
 } else {
-    $searched = false;
+    $authenticated = false;
 }
 ?>
 
@@ -13,22 +24,24 @@ if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["txtTukhoa"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tìm sách</title>
+    <title>Đăng nhập</title>
 </head>
 <body>
-    <h1>TÌM SÁCH</h1>
+    <h1>ĐĂNG NHẬP</h1>
 
-    <?php if ($searched) { ?>
-        <p>Từ khóa tìm sách là: <?= $sTukhoa ?></p>
-        <p>Kết quả tìm là:</p>
-        <ul>
-            <!-- Thêm logic hiển thị kết quả tìm kiếm ở đây -->
-        </ul>
+    <?php if ($authenticated) { ?>
+        <p <?= $messageStyle ?>><?= $message ?></p>
     <?php } else { ?>
         <form action="index.php" method="GET">
-            <label for="txtTukhoa">Từ khóa:</label>
-            <input type="text" id="txtTukhoa" name="txtTukhoa">
-            <input type="submit" value="Tìm">
+            <label for="txtTenTaiKhoan">Nhập tài khoản:</label>
+            <input type="text" id="txtTenTaiKhoan" name="txtTenTaiKhoan">
+            <br>
+            <br>
+            <label for="txtMatKhau">Nhập mật khẩu:</label>
+            <input type="password" id="txtMatKhau" name="txtMatKhau">
+            <br>
+            <br>
+            <input type="submit" value="Đăng nhập">
         </form>
     <?php } ?>
 </body>
