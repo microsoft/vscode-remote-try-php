3d28df3103b7f8d50182dcf54ad5222f1151c92d
nmhung03 <manhhung3072003@gmail.com>
2024-04-20 19:50:04 +0700

    submit

---
+++ login.php
@@ -0,0 +1,11 @@
+	<form method="POST" >
+    <div class="mb-3">
+      <label for="inputUsername" class="form-label">Tên đăng nhập</label>
+      <input type="text" name="username" class="form-control" id="inputUsername">
+    </div>
+    <div class="mb-3">
+      <label for="inputPassword" class="form-label">Mật khẩu</label>
+      <input type="password" class="form-control" id="inputPassword">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
