2479b6babd96758ba872ba4746b0ee7d2709766a
DucAnh <anhnguyen2k373@gmail.com>
2024-04-25 09:51:08 +0700

    Thiết kế button thêm, xóa sửa

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;0,900;1,300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/dc2a236791.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="../img/fa.ico" type="image/x-icon">
     <title>Thông tin Sinh viên</title>
     <style>
         body {
@@ -36,7 +39,7 @@
             border: 1px solid #ddd;
         }
         th, td {
-            padding: 12px;
+            padding: 10px;
             text-align: left;
         }
         th {
@@ -49,14 +52,14 @@
         }
         td a.delete-btn {
             display: inline-block;
-            padding: 8px 12px;
-            background-color: red;
+            padding: 4px 6px;
+            /* background-color: rgba(0, 0, 0.1, 0.1); */
             color: #fff;
-            border-radius: 5px;
+            border-radius: 50px;
             text-decoration: none;
         }
         td a.delete-btn:hover {
-            background-color: #dc3545;
+            background-color: #0288d10a;
         }
         td a.edit-btn {
             display: inline-block;
@@ -85,6 +88,92 @@
         a.add-btn:hover {
             background-color: #0056b3; /* Màu xanh lá cây nhạt khi di chuột vào */
         }
+        tr:hover {
+            background-color: #f6f6f6; /* Red background on hover */
+        }
+
+        *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif; 
+}
+
+a{
+    color: white;
+    text-decoration: none;
+}
+
+body{
+    width: 1519px;
+}
+
+
+
+.top-header{
+    height: 50px;
+    display: flex;
+    /* padding: 0 145px; */
+    justify-content:space-between;
+    color: #333333;
+    line-height: 25.6px;
+    background-color: #ededed;
+}
+
+.top-header-title{
+    padding-left: 15px;
+}
+
+.top-header-list{
+    padding-right: 15px;
+}
+
+.top-header-list>ul>li>a:hover{
+    color: red;
+}
+
+.top-header-list ul{
+    align-items: center;
+    color: #333333;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    line-height: 25.6px;
+    list-style: none;
+}
+
+.top-header-list ul li{
+    height: 15px;
+    margin-top:5px;
+    padding: 0 10px;
+    border-right: 1px solid #ccc;
+}
+
+.top-header-list ul li:last-child{
+    border: none;
+}
+
+.top-header-title a{
+    color: #484848;
+    font-family: 'Roboto', sans-serif;
+    font-size: 13px;
+    font-weight: 700;
+    line-height: 22.4px;
+    text-align: left;
+}
+
+.top-header-list a{
+    height: 20px;
+    padding: 8 0px;
+    color:#4d4646;
+    flex-wrap:wrap;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 14px;
+    line-height: 22.4px;
+    text-align:left;
+
+}
+        
     </style>
 </head>
 <body>
@@ -101,8 +190,19 @@
                 window.location.href = 'index.php';
         }
     </script>
-
+    
     <div class="container">
+        <div class="top-header">
+            <div class="top-header-title"><a href="">Tổng số: 15</a></div>
+            <div class="top-header-list">
+                <ul>
+                    <li><a href="#"><i class="fa-solid fa-chart-simple fa-sm" style="color: #f26526;"></i></a></li>
+                    <li><a href="#"><i class="fa-solid fa-filter fa-sm" style="color: #0288d1;"></i></li>
+                    <li><a href="#"><i class="fa-solid fa-filter-circle-xmark fa-sm" style="color: #f26526;"></i></a></li>
+                    <li><a href="#"><i class="fa-regular fa-circle-down fa-sm" style="color: #0288d1;"></i></i></a></li>
+                </ul>
+            </div>
+        </div>
         <?php
         $host = "localhost";
         $user = "root";
@@ -142,8 +242,9 @@
                 echo "<td>" . $row['Email'] . "</td>";
                 echo "<td>" . $row['DiaChi'] . "</td>";
                 echo "<td>";
-                echo "<a href='edit.php?MSSV=" . $row['MSSV'] . "' class='edit-btn'>Edit</a>";
-                echo "<a href='#' class='delete-btn' onclick='confirmDelete(" . $row['MSSV'] . ")'>Delete</a>";
+                echo "<a href='#' class='delete-btn'> <i class='fa-solid fa-up-right-from-square fa-sm' style='color: #0288d1;'></i></a>";
+                echo "<a href='edit.php?MSSV=" . $row['MSSV'] . "' class='delete-btn'><i class='fa-solid fa-file-pen fa-sm' style='color: #0288d1;'></i></a>";
+                echo "<a href='#' class='delete-btn' onclick='confirmDelete(" . $row['MSSV'] . ")'><i class='fa-solid fa-trash fa-sm' style='color: #f0284a;'></i></a>";
                 echo "</td>";
                 echo "</tr>";
             }
