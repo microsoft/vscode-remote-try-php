3f02a9d57b05e10ce93f083d9767ce322c96a4a1
vuminhphong23 <phongvuminh2003@gmail.com>
2024-05-05 21:07:30 +0700

    update upload.php

--- btap_tren_lop/Controller/upload.php
+++ btap_tren_lop/Controller/upload.php
@@ -21,14 +21,14 @@ if ($_FILES["fileToUpload"]["size"] > 5000000) {
 // Kiểm tra biến $uploadOk có bị đặt thành 0 không do có lỗi
 if ($uploadOk == 0) {
     // Chuyển hướng người dùng trở lại trang index.php với thông báo lỗi
-    header("Location: ../View/index.php?error=$errorMessage");
+    header("Location: ../View/files.php?error=$errorMessage");
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         // Chuyển hướng người dùng trở lại trang index.php với thông báo tải lên thành công
-        header("Location: ../View/index.php?success=The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.");
+        header("Location: ../View/files.php?success=The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.");
     } else {
         // Chuyển hướng người dùng trở lại trang index.php với thông báo lỗi khi tải lên
-        header("Location: ../View/index.php?error=Sorry, there was an error uploading your file.");
+        header("Location: ../View/files.php?error=Sorry, there was an error uploading your file.");
     }
 }
 ?>
