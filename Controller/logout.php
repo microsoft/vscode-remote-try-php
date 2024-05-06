8af82e68de0f4c06f799f5a2b75c5fb7268cc566
KhuatKien <kien18122k3@gmail.com>
2024-05-06 13:27:32 +0700

    .

--- Controller/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-// Đặt trạng thái login thành false và xóa session
-$_SESSION["IsLogin"] = false;
-session_destroy();
-// Redirect về trang đăng nhập
-header("Location: login.htm");
-?>
