99634e7adfaa5855e9f0e3071ae1d9952960f612
nmhung03 <manhhung3072003@gmail.com>
2024-05-09 01:58:05 +0700

    upgrade-database

--- baiTap/tuan3/databases/db_test.sql
+++ baiTap/tuan3/databases/db_test.sql
@@ -23,7 +23,8 @@ USE `db_test`;
 CREATE TABLE IF NOT EXISTS `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `path` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `path` (`path`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table db_test.images: ~0 rows (approximately)
