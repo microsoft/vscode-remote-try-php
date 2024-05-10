ea437bbaf9b7e15168b9e6bafdb6be26ce087b92
Anh Hoang <21012034@st.phenikaa-uni.edu.vn>
2024-05-10 15:49:48 +0700

    nopbt

---
+++ BT/Tuan 3/Bai1/welcome.php
@@ -0,0 +1,17 @@
+<?php include 'protect.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+</head>
+<body>
+    <h1>Trang chủ</h1>
+
+    <p>Chào mừng bạn đến với trang web của chúng tôi!</p>
+    <div class="input-group">
+        <input type="submit" value="Logout" a="logout.php">
+    </div>
+</body>
+</html>
